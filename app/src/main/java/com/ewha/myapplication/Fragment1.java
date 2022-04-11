27a7b4b9ddfad5d0d8090dbf4bb4d688b68904c7
hannah0125 <jhn0125@ewhain.net>
2022-04-11 21:57:13 +0900

    프래그먼트 화면 구성

---
+++ app/src/main/java/com/ewha/myapplication/Fragment1.java
@@ -0,0 +1,28 @@
+package com.ewha.myapplication;
+
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import android.widget.MediaController;
+import android.widget.VideoView;
+
+import android.net.Uri;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class Fragment1 extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment1, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/ewha/myapplication/Fragment2.java
@@ -0,0 +1,100 @@
+package com.ewha.myapplication;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.github.mikephil.charting.charts.BarChart;
+
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
+
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+
+import com.github.mikephil.charting.utils.ColorTemplate;
+import androidx.fragment.app.Fragment;
+
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+public class Fragment2 extends Fragment {
+    BarChart chart;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment2, container, false);
+
+        initUI(rootView);
+
+        return rootView;
+    }
+
+    private void initUI(ViewGroup rootView) {
+        chart = rootView.findViewById(R.id.barchart);
+        chart.setDrawValueAboveBar(true);
+
+        // 확대 x
+        chart.setTouchEnabled(false);
+
+        chart.getDescription().setEnabled(false);
+        chart.setDrawGridBackground(false);
+
+        XAxis xAxis = chart.getXAxis();
+        xAxis.setEnabled(false);
+
+        YAxis leftAxis = chart.getAxisLeft();
+        leftAxis.setLabelCount(6, false);
+        leftAxis.setAxisMinValue(0.0f);
+        leftAxis.setGranularityEnabled(true);
+        leftAxis.setGranularity(1f);
+
+
+        YAxis rightAxis = chart.getAxisRight();
+        rightAxis.setEnabled(false);
+
+        Legend legend2 = chart.getLegend();
+        legend2.setEnabled(false);
+
+        chart.animateXY(1500, 1500);
+
+        setData2();
+    }
+
+    private void setData2() {
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+
+        entries.add(new BarEntry(1.0f, 20));
+        entries.add(new BarEntry(2.0f, 40));
+        entries.add(new BarEntry(3.0f, 60));
+        entries.add(new BarEntry(4.0f, 30));
+        entries.add(new BarEntry(5.0f, 90));
+
+        BarDataSet dataSet2 = new BarDataSet(entries, "Sinus Function");
+        dataSet2.setColor(Color.rgb(240, 120, 124));
+
+        ArrayList<Integer> colors = new ArrayList<>();
+        for (int c : ColorTemplate.LIBERTY_COLORS) {
+            colors.add(c);
+        }
+        dataSet2.setColors(colors);
+
+        BarData data = new BarData(dataSet2);
+        data.setValueTextSize(10f);
+        data.setDrawValues(false);
+        data.setBarWidth(0.8f);
+
+        chart.setData(data);
+        chart.invalidate();
+    }
+
+
+}
