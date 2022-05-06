cc00a777987d805d52490968ee6df0b2be762772
hannah0125 <jhn0125@ewhain.net>
2022-05-06 16:04:50 +0900

    webview 삭제 / aws IVS 통한 playerview 사용

--- app/src/main/java/com/ewha/myapplication/Fragment1.java
+++ app/src/main/java/com/ewha/myapplication/Fragment1.java
@@ -29,27 +29,10 @@ import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-// 프래그먼트 1 서버 소켓 test
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
-import java.net.ServerSocket;
-import java.net.Socket;
-import java.net.InetAddress;
-import java.lang.Runnable;
+// AWS IVS VIEWER
+import com.amazonaws.ivs.player.*;
 
 public class Fragment1 extends Fragment {
-
-
-    // 프래그먼트 1 서버 소켓 test
-    EditText editText;
-    TextView textView2;
-    TextView textView3;
-    Handler handler = new Handler();
-
-    // 웹뷰 test
-    WebView webview1;
-    WebSettings webSettings;
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         ViewGroup rootView = (ViewGroup) inflater.inflate(R.layout.fragment1, container, false);
@@ -59,29 +42,14 @@ public class Fragment1 extends Fragment {
         return rootView;
     }
 
+    // AWS IVS VIEWER
+    PlayerView playerView;
+    Player player;
 
     private void initUI(ViewGroup rootView) {
-        webview1 = (WebView) rootView.findViewById(R.id.webview1);
-        webview1.setWebViewClient(new WebViewClientClass());
+        playerView = rootView.findViewById(R.id.playerView);
+        player = playerView.getPlayer();
 
-        webSettings = webview1.getSettings();
-        webSettings.setJavaScriptEnabled(true);
-        webSettings.setJavaScriptCanOpenWindowsAutomatically(true);
-        webview1.loadUrl("http://10.0.2.2:8080");
-    }
-
-    private class WebViewClientClass extends WebViewClient {
-        // SSL 인증서 무시
-        @Override
-        public void onReceivedSslError(WebView view, SslErrorHandler handler, SslError error) {
-            handler.proceed();
-        }
-
-        // 페이지 내에서만 url 이동하게끔 만듬
-        @Override
-        public boolean shouldOverrideUrlLoading(WebView view, String url) {
-            view.loadUrl(url);
-            return true;
-        }
+        player.load(Uri.parse("https://fcc3ddae59ed.us-west-2.playback.live-video.net/api/video/v1/us-west-2.893648527354.channel.DmumNckWFTqz.m3u8"));
     }
 }
